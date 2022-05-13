7b64ef46303f565c59701a4bb0497551724a631f
Boakai <boakaidkamara@gmail.com>
2022-05-13 01:36:16 +0000

    added main menu to ATM app

--- atm.py
+++ atm.py
@@ -7,10 +7,32 @@ user = input("Enter username \n")
 # get index for current user 
 user_index = user.index(user)
 
+
 if(user in users):
+    #prompt for password
     password = input("Enter your password \n")
+
     if(password == passwords[user_index]):
-        print("Welcome")
+        print("Welcome \n")
+
+        # display options 
+        print("What do you want to do? Enter a number to select an option")
+        print("1. Withdraw \n")
+        print("2. Save Money \n")
+        print("3. Complaints \n")
+
+        selected_option = int(input())
+        print(selected_option)
+
+        if(selected_option == 1):
+            print("Withdraw")
+        elif(selected_option == 2):
+            print("Saving")
+        elif(selected_option == 3):
+            print("Complaints")
+        else:
+            print("Invalid option, try again with a valid option")
+
     else:
         print("Password not correct")
 else:
